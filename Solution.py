7d73038c8d35b2b415a2c091c3d9e916b0e9fb2b
kumar-kanujia <70059467+kumar-kanujia@users.noreply.github.com>
2021-09-16 14:07:47 +0530

    first commit

---
+++ Solution.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+from PIL import Image
+import os
+import numpy as np
+import random
+import tensorflow as tf
+
+model = tf.keras.models.load_model('saved_model/my_model')
+
+test=[]
+test_dir_files = os.listdir('test')
+for img_name in  test_dir_files:
+    img=Image.open('test/'+img_name)
+    img=np.array(img)
+    test.append(img)
+test=np.array(test)/255
+
+result=np.argmax(model.predict(test),axis=1)
+
+
+# In[5]:
+
+
+import random
+import json
+def write_json(filename, result):
+    with open(filename, 'w') as outfile:
+        json.dump(result, outfile)
+
+def read_json(filename):
+    with open(filename, 'r') as outfile:
+        data =  json.load(outfile)
+    return data
+
+def generate_sample_file(filename):
+    res = {}
+    for i in range(1,100):
+        test_set = str(i) + '.png'
+        res[test_set] = 3
+    return res
+    #write_json(filename, res)
+
+if __name__ == '__main__':
+    generate_sample_file('./sample_result1.json')
+
+
+# In[10]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
